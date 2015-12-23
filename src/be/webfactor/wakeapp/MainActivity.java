5fa006cf287f1b6ecd91c7393d272b6301690957
Peter Mesotten <peter.mesotten@aca-it.be>
2015-12-23 15:15:44 +0100

    #1: Remove title bar and status bar

--- src/be/webfactor/wakeapp/MainActivity.java
+++ src/be/webfactor/wakeapp/MainActivity.java
@@ -2,6 +2,8 @@ package be.webfactor.wakeapp;
 
 import android.app.Activity;
 import android.os.Bundle;
+import android.view.Window;
+import android.view.WindowManager;
 
 public class MainActivity extends Activity {
 	/**
@@ -10,6 +12,10 @@ public class MainActivity extends Activity {
 	@Override
 	public void onCreate(Bundle savedInstanceState) {
 		super.onCreate(savedInstanceState);
+
+		requestWindowFeature(Window.FEATURE_NO_TITLE);
+		getWindow().setFlags(WindowManager.LayoutParams.FLAG_FULLSCREEN, WindowManager.LayoutParams.FLAG_FULLSCREEN);
+
 		setContentView(R.layout.main);
 	}
 }
