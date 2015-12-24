22ec30c729b28be2f5ac60ed28a61b97c277658b
Peter Mesotten <peter.mesotten@aca-it.be>
2015-12-24 14:13:30 +0100

    #1: Clicking on image switches image

--- src/be/webfactor/wakeapp/MainActivity.java
+++ src/be/webfactor/wakeapp/MainActivity.java
@@ -2,13 +2,15 @@ package be.webfactor.wakeapp;
 
 import android.app.Activity;
 import android.os.Bundle;
+import android.view.View;
 import android.view.Window;
 import android.view.WindowManager;
+import android.widget.ImageView;
 
 public class MainActivity extends Activity {
-	/**
-	 * Called when the activity is first created.
-	 */
+
+	private int current = R.drawable.night;
+
 	@Override
 	public void onCreate(Bundle savedInstanceState) {
 		super.onCreate(savedInstanceState);
@@ -17,5 +19,17 @@ public class MainActivity extends Activity {
 		getWindow().setFlags(WindowManager.LayoutParams.FLAG_FULLSCREEN, WindowManager.LayoutParams.FLAG_FULLSCREEN);
 
 		setContentView(R.layout.main);
+
+		final ImageView image = (ImageView) findViewById(R.id.image);
+		image.setOnClickListener(new View.OnClickListener() {
+			public void onClick(View view) {
+				if (current == R.drawable.day) {
+					current = R.drawable.night;
+				} else {
+					current = R.drawable.day;
+				}
+				image.setImageResource(current);
+			}
+		});
 	}
 }
